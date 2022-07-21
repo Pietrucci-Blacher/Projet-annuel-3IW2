6920dbcecbb58b454bfd32e79c242a10ec2aa6bf
benjaminli7 <nicky01.bl@gmail.com>
2022-07-21 22:21:52 +0200

    revert files

--- www/conf.inc.php
+++ www/conf.inc.php
@@ -1,17 +1,8 @@
 <?php
 
-if ($_SERVER['SERVER_NAME'] === 'localhost') {
-    define("DBUSER", "root");
-    define("DBPWD", "password");
-    define("DBHOST", "database");
-
-} else if ($_SERVER['SERVER_NAME'] === 'chiperz.fr') {
-    define("DBUSER", "admin5978569");
-    define("DBPWD", "14LNhCcIl7XlahGufU1");
-    define("DBHOST", "localhost");
-}
-
-
+define("DBUSER", "root");
+define("DBPWD", "password");
+define("DBHOST", "database");
 define("DBNAME", "chiperz");
 define("DBDRIVER", "mysql");
 define("DBPORT", "3306");
