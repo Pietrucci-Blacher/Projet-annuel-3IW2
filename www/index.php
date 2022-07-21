9f1113e234fad3abcfd95cf1391a414dea95de51
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2022-07-22 00:28:59 +0200

    Add Debug

--- www/index.php
+++ www/index.php
@@ -1,21 +1,25 @@
 <?php
 namespace App;
+use App\Core\Config;
 use App\Core\Router;
 
 use App\Core\AutoLoader;
 
 require 'Core/AutoLoader.php';
+require 'conf.inc.php';
 
 AutoLoader::init();
 
 session_start();
 
-/*if(Config::getInstance()->get("dev_debug")) {
+if(Config::getInstance()->get("dev_debug")) {
     ini_set('display_errors', 1);
 }else{
-    ini_set('display_errors', 0)git;
-}*/
+    ini_set('display_errors', 0);
+}
 
 $uri = strtok($_SERVER["REQUEST_URI"],'?');
 
 $router = new Router($uri);
+
+
